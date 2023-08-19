5114c593ccff8c594cce626d84e0557e48059348
Blueline Learning <tomislavdivljak92@gmail.com>
2023-08-19 20:20:14 +0200

    Updated the app functionality

--- quotes.py
+++ quotes.py
@@ -5,7 +5,7 @@ import requests
 import psycopg2
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:sifrazapostgre@localhost/quotes"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:sifrazapostgres@localhost/quotes"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 db = SQLAlchemy(app)
 
